b600cc0fd4c94ed4c0af752801ab59caa0a401aa
francoandre.dev <francoandre.dev@gmail.com>
2023-11-05 19:14:20 -0500

    init project

--- main.js
+++ main.js
@@ -1,2 +1,35 @@
-const app = document.querySelector("body>.app");
-console.log(app);
+function fillDataWithJson() {
+    function fillAboutData(query, jsonPath) {
+        const [...paragraphs] = document.querySelectorAll(query);
+
+        fetch(jsonPath)
+            .then((res) => res.json())
+            .then((res) => {
+                paragraphs.forEach((p, index) => {
+                    p.textContent = res[index];
+                });
+            })
+            .catch((err) => console.error(err));
+    }
+
+    function fillDataComments(query, subQuery, jsonPath) {
+        const [...cards] = document.querySelectorAll(query);
+
+        fetch(jsonPath)
+            .then((res) => res.json())
+            .then((res) => {
+                cards.forEach((card, index) => {
+                    const [...paragraphs] = card.querySelectorAll(subQuery);
+                    paragraphs[0].textContent = res[index].name;
+                    paragraphs[1].textContent = res[index].time;
+                    paragraphs[2].textContent = res[index].comment;
+                });
+            })
+            .catch((err) => console.error(err));
+    }
+
+    fillAboutData("#nosotros>p.about-us", "./data/about.json");
+    fillDataComments("#opiniones .card.published", "p", "./data/comments.json");
+}
+
+fillDataWithJson();
