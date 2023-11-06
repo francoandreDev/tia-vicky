939e4df8be85e5cf6d7a748697180b95a672b029
francoandre.dev <francoandre.dev@gmail.com>
2023-11-05 19:57:12 -0500

    init project

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
 function fillDataWithJson() {
+    function fillProductsData(query, jsonPath) {
+        const allProducts = document.querySelector(query);
+        const [...productCards] = allProducts.children;
+        fetch(jsonPath)
+            .then((res) => res.json())
+            .then((res) => {
+                productCards.forEach((productCard, index) => {
+                    const price = productCard.querySelector(".price");
+                    const name = productCard.querySelector(".name");
+                    const category = productCard.querySelector(".category");
+                    const image =
+                        productCard.querySelector(".wrapper-image>img");
+                    const description =
+                        productCard.querySelector(".description");
+                    price.textContent = "S/. " + res[index].price;
+                    name.textContent = res[index].name;
+                    category.textContent = res[index].category;
+                    description.textContent = res[index].description;
+                    image.alt = "Imagen de " + res[index].name;
+                    image.src = res[index].image;
+                });
+            })
+            .catch((err) => console.error(err));
+    }
+
     function fillAboutData(query, jsonPath) {
         const [...paragraphs] = document.querySelectorAll(query);
 
@@ -28,6 +53,7 @@ function fillDataWithJson() {
             .catch((err) => console.error(err));
     }
 
+    fillProductsData("#productos>.products", "./data/menu.json");
     fillAboutData("#nosotros>p.about-us", "./data/about.json");
     fillDataComments("#opiniones .card.published", "p", "./data/comments.json");
 }
